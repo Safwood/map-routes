db859d86094bef620733d9bc356f2db697b06753
safwood <naste-f@yandex.ru>
2025-01-06 20:08:45 +0300

    add all necessary pages to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,57 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import MapPage from "../pages/MapPage.vue";
+import ParksPage from "../pages/ParksPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
+      children: [
+        {
+          name: "map",
+          path: "map",
+          component: MapPage,
+          children: [
+            {
+              name: "parks",
+              path: "parks",
+              component: ParksPage,
+            },
+            {
+              name: "stores",
+              path: "stores",
+              component: () => import("../pages/StoresPage.vue"),
+            },
+            {
+              path: "",
+              redirect: { name: "parks" },
+            },
+          ],
+        },
+        {
+          name: "about",
+          path: "about",
+          component: () => import("../pages/AboutPage.vue"),
+        },
+        {
+          path: "",
+          redirect: { name: "parks" },
+        },
+      ],
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/",
+      redirect: { name: "parks" },
+    },
+    {
+      path: "/:catchAll(.*)",
+      redirect: { name: "parks" },
+    },
+  ],
+});
 
-export default router
+export default router;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
+import { RouterLink } from "vue-router";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <header>
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/map/parks">Parks</RouterLink>
+          <RouterLink to="/map/stores">Stores</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+    </header>
+
+    <router-view />
   </main>
 </template>
